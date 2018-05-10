949be3a2b5a5943378f5d92010f57817f89dfb63
TerionGVS <Vveterokk@gmail.com>
2018-05-10 15:05:32 +1000

    В модели при создании нового поста осуществляется отправка письма подписчикам. В админке можно добавляться посты - письма также приходят. В настройках проекта: письма приходят на консоль (stdout)

--- UsersBlogs/app/models.py
+++ UsersBlogs/app/models.py
@@ -4,6 +4,7 @@ Definition of models.
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
 
 class Post(models.Model):
     owner = models.ForeignKey(User,on_delete=models.CASCADE)
@@ -12,6 +13,21 @@ class Post(models.Model):
     time = models.TimeField(auto_now = True)
     def __str__(self):
         return self.caption
+    # Отправка письма при добавлении поста, если указан email
+    def save(self, *args, **kwargs):
+        subs = Subscription.objects.all().filter(blogger=self.owner).values('user')
+        users = User.objects.all().filter(pk__in=subs)
+        super(Post, self).save(*args, **kwargs)
+        for u in users:
+            if u.email:
+                message = 'Here new post! http://localhost:8000/private_office/post_detail/{}/'.format(self.pk)
+                send_mail(
+                   'New post!',
+                   message,
+                   'BlogsUsers@example.com',
+                    [u.email],
+                fail_silently=False,
+                )
 
 class Subscription(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
